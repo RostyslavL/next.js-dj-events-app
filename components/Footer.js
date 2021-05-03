0d024bf3348e2a011347013133a30ef41383dd9e
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-03 13:26:03 +0200

    fix: space correction

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <p> Copyryght &copy; Rostyslav L 2021</p>
+            <p> Copyryght &copy; RostyslavL 2021</p>
             <Link href='/about'>
                 About this Project
             </Link>
