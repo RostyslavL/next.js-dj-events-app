91d2c103752bfd2ef9e7555037477fc46a9861d4
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-03 13:22:35 +0200

    feat:  create Layout , Header, Footer components with Footer.moduse.css, Header.module.css, add them into main app

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p> Copyryght &copy; Rostyslav L 2021</p>
+            <Link href='/about'>
+                About this Project
+            </Link>
+        </footer>
+    )
+}
