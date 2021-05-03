7c58902894ab41daeadec76d364db8dfd68c4940
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-03 11:21:53 +0200

    init: create first next.js repo for frontend with some pages initialised

---
+++ dj-events/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>
+                This is an app to find the latest musical events
+            </p>
+            <p>
+                <strong>Version 1.0.0</strong>
+            </p>
+        </div>
+    )
+}
