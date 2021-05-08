d4b4080c1976d71ff2947f3c612bebd05542f8ca
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-08 23:12:06 +0200

    feat: install cookie paclage with  passing cookie as a  prop in edit event page

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,6 @@
+import cookie from 'cookie'
 import { API_URL } from '@/config/index'
+
 
 export default async(req, res) =>{
     if(req.method === 'POST'){
@@ -19,9 +21,17 @@ export default async(req, res) =>{
         const data = await strapiRes.json()
 
     
-
         if(strapiRes.ok){
             // Set cookie
+            res.setHeader('Set-Cookie', cookie.serialize('token',  data.jwt, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                maxAge: 60 * 60 * 24 * 7, // 1 week || 
+                // maxAge: 60 * 60 * 24 * 7 * 4 , // 1 month 
+                sameSite: 'strict',
+                path: '/'
+            }))
+
             res.status(200).json({user: data.user})
         }else{
             res
