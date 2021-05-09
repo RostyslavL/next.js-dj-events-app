f8354438ed52441bd4120317a6aa6241aa065c1b
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-05-09 10:17:51 +0200

    feat: create Dashboard event page , Dashboard , fetching data events from strapi api to associated user

---
+++ components/DashboardEvent.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import {HiOutlineTrash, HiPencil, HiCalendar,HiChevronLeft} from 'react-icons/hi'
+import styles from '@/styles/DashboardEvent.module.css'
+
+
+export default function DashboardEvent({evt, handleDelete}) {
+    return (
+        <div className={styles.event}>
+            <h4>
+                <Link href={`/events/${evt.slug}`}>
+                    <a>{evt.name}</a>
+                </Link>
+            </h4>
+            <Link href={`/events/evt/${evt.id}`}>
+                    <a className={styles.edit}>
+                        <HiPencil /> <span> Edit Event</span>
+                    </a>
+                </Link>
+                <a className={styles.delete} onClick={() => handleDelete(evt.id)}>
+                        <HiOutlineTrash /> <span> Delete Event</span>
+                </a>
+        </div>
+    )
+}
